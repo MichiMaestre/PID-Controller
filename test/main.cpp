a095a4eeef44d1214b1924a544042ecc7069bd67
Miguel Maestre <a17maestre2009@gmail.com>
2017-09-23 17:27:41 -0400

    Add Copyright comment to main.cpp tests

--- test/main.cpp
+++ test/main.cpp
@@ -1,5 +1,9 @@
 #include <gtest/gtest.h>
 
+/*
+ * "Copyright [year] <Copyright Owner>"
+ */
+
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
